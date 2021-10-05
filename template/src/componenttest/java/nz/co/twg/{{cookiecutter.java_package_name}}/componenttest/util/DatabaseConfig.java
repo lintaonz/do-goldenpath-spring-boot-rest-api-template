21e8228984718c95a7fc47ca00613c5b62af34cf
Tim Keegan <184611@thewarehouse.co.nz>
2021-10-05 14:42:22 +1300

    feat: Add Code Style (#PPD-1391)
    - Adds cross editor compatible config file
    - Adds Checkstyle plugin for detecting issues with code style
    - Adds Spotless plugin for detecting and resolving issues with code style
    - Fixes existing formatting issues to align with new styles
    
    Related: https://bitbucket.org/twgnz/do-goldenpath-spring-boot-rest-api/pull-requests/12

--- template/src/componenttest/java/nz/co/twg/{{cookiecutter.java_package_name}}/componenttest/util/DatabaseConfig.java
+++ template/src/componenttest/java/nz/co/twg/{{cookiecutter.java_package_name}}/componenttest/util/DatabaseConfig.java
@@ -6,30 +6,32 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
 
-	private static Connection connection;
+    private static Connection connection;
 
-	public DatabaseConfig() throws SQLException
-	{
-		String dbUser = System.getProperty("SPRING_DATASOURCE_USERNAME", "myuser");
-		//Jenkins pulls from command line args. Reading from env variable
-		String dbUrl = System.getProperty("SPRING_DATASOURCE_URL", "jdbc:postgresql://localhost:"+System.getProperty("db_port","30005")+"/mydb");
-		String dbPassword = System.getProperty("SPRING_DATASOURCE_PASSWORD", "mypassword");
-		connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-	}
+    public DatabaseConfig() throws SQLException {
+        String dbUser = System.getProperty("SPRING_DATASOURCE_USERNAME", "myuser");
+        // Jenkins pulls from command line args. Reading from env variable
+        String dbUrl =
+                System.getProperty(
+                        "SPRING_DATASOURCE_URL",
+                        "jdbc:postgresql://localhost:" + System.getProperty("db_port", "30005") + "/mydb");
+        String dbPassword = System.getProperty("SPRING_DATASOURCE_PASSWORD", "mypassword");
+        connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    }
 
-	/*
-	 * public static DatabaseConfig getInstance() throws SQLException {
-	 * if(databaseConfig==null) { databaseConfig=new DatabaseConfig(); } return
-	 * databaseConfig;
-	 *
-	 * }
-	 */
+    /*
+    * public static DatabaseConfig getInstance() throws SQLException {
+    * if(databaseConfig==null) { databaseConfig=new DatabaseConfig(); } return
+    * databaseConfig;
+    *
+    * }
+    */
 
-	public Connection getConnection() {
-		return connection;
-	}
+    public Connection getConnection() {
+        return connection;
+    }
 
-	public void setConnection(Connection con) {
-		DatabaseConfig.connection = con;
-	}
+    public void setConnection(Connection con) {
+        DatabaseConfig.connection = con;
+    }
 }
