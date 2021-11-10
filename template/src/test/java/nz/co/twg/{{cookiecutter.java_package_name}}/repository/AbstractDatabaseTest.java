68e3df9a23c8f562f43ca37219d893ba23978aea
I-Yang Huang <316557@twgroup.co.nz>
2021-11-10 16:07:51 +1300

    feat: Add sample database integration tests (PPD-2435)

---
+++ template/src/test/java/nz/co/twg/{{cookiecutter.java_package_name}}/repository/AbstractDatabaseTest.java
@@ -0,0 +1,82 @@
+package nz.co.twg.{{cookiecutter.java_package_name}}.repository;
+
+import java.util.Collections;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import javax.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+* A base database test that initializes a containerized db instance and a spring context. All
+* database related tests should extend off this class to share the same db instance.
+*/
+@ActiveProfiles("local")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = AbstractDatabaseTest.DockerPostgreDataSourceInitializer.class)
+@Testcontainers
+public abstract class AbstractDatabaseTest {
+
+    private static final PostgreSQLContainer<?> postgreDBContainer =
+            new PostgreSQLContainer<>("postgres:12-alpine")
+                    .withTmpFs(Collections.singletonMap("/var/lib/postgresql/data", "rw"));
+
+    static {
+        postgreDBContainer.start();
+    }
+
+    static class DockerPostgreDataSourceInitializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext,
+                    "spring.jpa.hibernate.ddl-auto = create-drop",
+                    "spring.datasource.url = " + postgreDBContainer.getJdbcUrl(),
+                    "spring.datasource.username = " + postgreDBContainer.getUsername(),
+                    "spring.datasource.password = " + postgreDBContainer.getPassword());
+        }
+    }
+
+    @Autowired private EntityManager em;
+
+    @Autowired private TransactionTemplate txnTemplate;
+
+    /**
+    * A wrapper so the wrapped code executes within a valid transaction boundary. Exposing the entity
+    * manager directly to bypass the DAO under test so the testing of the DAO can be more isolated
+    * when comes to setting up default data. <br>
+    * Use this method if return value is of no interest. <br>
+    * e.g. during data setup / teardown
+    */
+    protected void useTransaction(Consumer<EntityManager> entityManagerConsumer) {
+        txnTemplate.execute(
+                (status) -> {
+                    entityManagerConsumer.accept(em);
+                    return null;
+                });
+    }
+
+    /**
+    * A wrapper so the wrapped code executes within a valid transaction boundary. Exposing the entity
+    * manager directly to bypass the DAO under test so the testing of the DAO can be more isolated
+    * when comes to setting up default data. <br>
+    * Use this method if a result must be returned. <br>
+    * e.g. during the "when" query with result just before "then" assertions
+    */
+    protected <T> T withTransaction(Function<EntityManager, T> entityManagerConsumer) {
+        return txnTemplate.execute((status) -> entityManagerConsumer.apply(em));
+    }
+}
---
+++ template/src/test/java/nz/co/twg/{{cookiecutter.java_package_name}}/repository/PetRepositoryTest.java
@@ -0,0 +1,95 @@
+package nz.co.twg.{{cookiecutter.java_package_name}}.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import nz.co.twg.{{cookiecutter.java_package_name}}.entity.Pet;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/** This is an example test for database integration test. */
+class PetRepositoryTest extends AbstractDatabaseTest {
+
+    @Autowired PetRepository petRepository;
+
+    @BeforeEach
+    public void setUp() {
+        useTransaction((entityManager) -> petRepository.deleteAll());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        useTransaction((entityManager) -> petRepository.deleteAll());
+    }
+
+    @Test
+    void testSaveAndCount() {
+        // given
+        assertEquals(0, petRepository.count());
+        useTransaction(
+                (entityManager) -> {
+                    entityManager.persist(new Pet(1L, "alice", "dog"));
+                    entityManager.persist(new Pet(2L, "bob", "dog"));
+                    entityManager.persist(new Pet(3L, "carl", "cat"));
+                });
+
+        // when
+        long count = petRepository.count();
+
+        // then
+        assertEquals(3, count);
+    }
+
+    @Test
+    void testFindByTag() {
+        // given
+        assertEquals(0, petRepository.count());
+
+        Pet pet1 = new Pet();
+        pet1.setId(1L);
+        pet1.setName("alice");
+        pet1.setTag("dog");
+
+        Pet pet2 = new Pet();
+        pet2.setId(2L);
+        pet2.setName("bob");
+        pet2.setTag("dog");
+
+        Pet pet3 = new Pet();
+        pet3.setId(3L);
+        pet3.setName("carl");
+        pet3.setTag("cat");
+
+        useTransaction(
+                (entityManager) -> {
+                    entityManager.persist(pet1);
+                    entityManager.persist(pet2);
+                    entityManager.persist(pet3);
+                });
+
+        // when
+        List<Pet> result = petRepository.findByTag("dog");
+
+        // then
+        assertEquals(2, result.size());
+
+        Map<Long, Pet> petById = result.stream().collect(Collectors.toMap(Pet::getId, x -> x));
+
+        Pet result1 = petById.get(1L);
+        assertNotNull(result1);
+        assertEquals("alice", result1.getName());
+        assertEquals("dog", result1.getTag());
+
+        Pet result2 = petById.get(2L);
+        assertNotNull(result2);
+        assertEquals("bob", result2.getName());
+        assertEquals("dog", result2.getTag());
+
+        Pet result3 = petById.get(3L);
+        assertNull(result3);
+    }
+}
