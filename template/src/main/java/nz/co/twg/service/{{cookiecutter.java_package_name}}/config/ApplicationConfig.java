463478056fbe6a6e94cc51f19547cb70add2f313
Mustansar Anwar ul Samad <229937@twgroup.co.nz>
2022-01-20 12:48:25 +1300

    feat: Add CORS for backstage (PPD-5127)
    This will allow teams to explore APIs in backstage using
    OpenAPI UI

--- template/src/main/java/nz/co/twg/service/{{cookiecutter.java_package_name}}/config/ApplicationConfig.java
+++ template/src/main/java/nz/co/twg/service/{{cookiecutter.java_package_name}}/config/ApplicationConfig.java
@@ -1,6 +1,28 @@
 package nz.co.twg.service.{{cookiecutter.java_package_name}}.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ApplicationConfig {}
+public class ApplicationConfig {
+    /**
+    * This CORS policy allows teams to call the API using the OpenAPI UI in backstage - from both dev
+    * and prod instances. Do not remove this as we want all teams to to be able to explore APIs in
+    * backstage for dev and test environments.
+    *
+    * @return -
+    */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("https://backstage-dev.twg.co.nz", "https://backstage.twg.co.nz");
+            }
+        };
+    }
+}
